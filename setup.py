b43530748e731f58ac63066a65c02c8fde7ab804
kartheek23 <challakartheek@gmail.com>
2024-06-26 14:13:23 +0530

    new files added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup (
+    name='mcqgenerator',
+    version='0.0.1',
+    author='Kartheek Challa',
+    author_email='challakartheek@gmail.com',
+    install_requires=['openai','langchain','streamlit','python-dotenv','pypdf2','pandas'],
+    packages=find_packages()
+)
