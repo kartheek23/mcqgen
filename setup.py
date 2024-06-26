ef55a96f780e3bc08ae4660dd05c3fa34b761966
kartheek23 <challakartheek@gmail.com>
2024-06-26 17:50:36 +0530

    file updated

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup (
     version='0.0.1',
     author='Kartheek Challa',
     author_email='challakartheek@gmail.com',
-    install_requires=['openai','langchain','streamlit','python-dotenv','pypdf2','pandas'],
+    install_requires=['openai','langchain','streamlit','python-dotenv','pypdf2','pandas','langchain-community'],
     packages=find_packages()
 )
